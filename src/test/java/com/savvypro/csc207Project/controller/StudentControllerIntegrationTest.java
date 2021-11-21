935a8b3757b87d4b92752b527caa345c8fc7d9d9
Chris Lu <chrislu0126@hotmail.com>
2021-11-21 11:08:02 -0500

    Further make test better

--- src/test/java/com/savvypro/csc207Project/controller/StudentControllerIntegrationTest.java
+++ src/test/java/com/savvypro/csc207Project/controller/StudentControllerIntegrationTest.java
@@ -2,50 +2,53 @@ package com.savvypro.csc207Project.controller;
 
 import com.savvypro.csc207Project.entity.Student;
 import com.savvypro.csc207Project.service.StudentService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(StudentController.class)
 public class StudentControllerIntegrationTest {
+    private static final String USERNAME = "username";
+    private static final String OLD_FIRST_NAME = "Chris";
+    private static final String OLD_LAST_NAME = "Lu";
+    private static final String NEW_FIRST_NAME = "Andy";
+    private static final String NEW_LAST_NAME = "Chu";
+
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private StudentService service;
+    private StudentService studentService;
+
+    @Before
+    public void setUp() {
+        Mockito.when(studentService.getStudentByUsername(USERNAME))
+                .thenReturn(Student.builder().username(USERNAME).firstName(OLD_FIRST_NAME).lastName(OLD_LAST_NAME).build());
+    }
 
     @Test
-    public void testUpdateStudent()
-            throws Exception {
-
-        Student student = new Student();
-        student.setUsername("Chris");
-        student.setPassword("password");
-        student.setFirstName("Chris");
-        student.setLastName("Lu");
-
-        when(service.getStudentByUsername("Chris")).thenReturn(student);
-
+    public void testUpdateStudent() throws Exception {
         MvcResult response = mvc.perform(post("/student/update")
-                .param("username", "Chris")
-                .param("firstName", "Chris")
-                .param("lastName", "Lu"))
+                .param("username", USERNAME)
+                .param("firstName", NEW_FIRST_NAME)
+                .param("lastName", NEW_LAST_NAME))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/html;charset=UTF-8"))
                 .andExpect(view().name("studentInfoPage")).andReturn();
 
-        assertTrue("Chris", response.getResponse().getContentAsString().contains("Chris"));
+        assertTrue(response.getResponse().getContentAsString().contains(NEW_FIRST_NAME));
+        assertTrue(response.getResponse().getContentAsString().contains(NEW_LAST_NAME));
     }
 }
--- src/test/java/com/savvypro/csc207Project/controller/StudentControllerUnitTest.java
+++ src/test/java/com/savvypro/csc207Project/controller/StudentControllerUnitTest.java
@@ -1,7 +1,6 @@
 package com.savvypro.csc207Project.controller;
 
 import com.savvypro.csc207Project.entity.Student;
-import com.savvypro.csc207Project.repository.StudentRepository;
 import com.savvypro.csc207Project.service.StudentService;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,12 +12,15 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.ui.Model;
 import org.springframework.validation.support.BindingAwareModelMap;
 
-import java.util.Optional;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StudentControllerUnitTest {
+    private static final String USERNAME = "username";
+    private static final String OLD_FIRST_NAME = "Chris";
+    private static final String OLD_LAST_NAME = "Lu";
+    private static final String NEW_FIRST_NAME = "Andy";
+    private static final String NEW_LAST_NAME = "Chu";
 
     @InjectMocks
     private StudentController studentController;
@@ -26,19 +28,20 @@ public class StudentControllerUnitTest {
     @Mock
     private StudentService studentService;
 
-
     @Before
     public void setUp() {
-        Mockito.when(studentService.getStudentByUsername("chris"))
-                .thenReturn(Student.builder().username("chris").build());
+        Mockito.when(studentService.getStudentByUsername(USERNAME))
+                .thenReturn(Student.builder().username(USERNAME).firstName(OLD_FIRST_NAME).lastName(OLD_LAST_NAME).build());
     }
 
     @Test
     public void testUpdateStudent() {
-        Student studentNewInfo = Student.builder().username("chris").firstName("Chris").lastName("Lu").build();
+        Student studentNewInfo = Student.builder().username(USERNAME).firstName(NEW_FIRST_NAME).lastName(NEW_LAST_NAME).build();
         Model model = new BindingAwareModelMap();
+
         studentController.updateStudent(studentNewInfo, model);
 
-        assertEquals("Chris", ((Student)model.getAttribute("student")).getFirstName());
+        assertEquals(NEW_FIRST_NAME, ((Student)model.getAttribute("student")).getFirstName());
+        assertEquals(NEW_LAST_NAME, ((Student)model.getAttribute("student")).getLastName());
     }
 }
